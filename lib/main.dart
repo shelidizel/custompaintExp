aed4a0880df0eefa418598744d00c5bcef9ddb40
unknown <tujuelinks@gmail.com>
2023-06-26 09:14:58 +0300

    some changes on the stack

--- lib/main.dart
+++ lib/main.dart
@@ -26,22 +26,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
-    return MouseRegion(
-      onEnter: (event) {
-        
-        
-      },
-      onHover: (event) {
-        final renderObject = context.findRenderObject() as RenderBox;
-        final localPosition = renderObject.globalToLocal(event.localPosition);
-        print(localPosition);
-      },
-      onExit: (event){
 
+    List<Offset> hoverPositions = [];
+    
+    return Listener(
+      onPointerHover: (event) {
+        setState(() {
+          hoverPositions.add(event.localPosition);
+        });
       },
       child: CustomPaint(
-        painter: DemoPainter(),
+        painter: DemoPainter(
+          hoverPositions: hoverPositions
+        ),
         
       ),
     );
@@ -49,25 +46,37 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class DemoPainter extends CustomPainter {
+  List<Offset> hoverPositions;
+  DemoPainter({required this.hoverPositions});
 
   bool isArcHovered = false;
 
   @override
-  void paint(Canvas canvas, Size size, {List<Offset>? hoverPositions}) {
+  void paint(Canvas canvas, Size size, ) {
 
     Offset center = Offset(size.width/2, size.height/2);
     var rect = Rect.fromCenter(center: center, width: 200, height: 200);
-    if(hoverPositions != null){
-     for (var hoverPosition in hoverPositions) {
-       if(rect.contains(hoverPosition)){
-        isArcHovered = true;
-       }
-     }
-    }
+    
     
     Paint paint = Paint()..color = isArcHovered? Colors.blue : Colors.red;
 
-    canvas.drawArc(rect, 0.0, pi/1.2, true, paint);
+   Path path = Path();
+   path.arcTo(rect, 0.0, pi / 1, true);
+
+   if(hoverPositions.isNotEmpty){
+    print("hoverPostions id not empty");
+     for (var hoverPosition in hoverPositions) {
+       if(path.contains(hoverPosition)){
+        isArcHovered = true;
+        print(hoverPosition);
+       }
+     }
+    } else {
+      print('hoverpositions is empty');
+    }
+
+    canvas.drawPath(path, paint);
+    
     
   }
 
