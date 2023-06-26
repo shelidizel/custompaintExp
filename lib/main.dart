c0cc5798e9568100745fc994250bafdfebbeb867
unknown <tujuelinks@gmail.com>
2023-06-26 22:38:09 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,19 +21,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  List<Offset> hoverPositions = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    hoverPositions.clear();
+    print('hover positions cleared');
+    super.initState();
+  }
+
   
  
 
   @override
   Widget build(BuildContext context) {
-
-    List<Offset> hoverPositions = [];
     
     return Listener(
       onPointerHover: (event) {
         setState(() {
           hoverPositions.add(event.localPosition);
         });
+          
+         
       },
       child: CustomPaint(
         painter: DemoPainter(
@@ -46,7 +57,8 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class DemoPainter extends CustomPainter {
-  List<Offset> hoverPositions;
+  
+  List<Offset> hoverPositions = [];
   DemoPainter({required this.hoverPositions});
 
   bool isArcHovered = false;
@@ -54,27 +66,47 @@ class DemoPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size, ) {
 
+
     Offset center = Offset(size.width/2, size.height/2);
     var rect = Rect.fromCenter(center: center, width: 200, height: 200);
     
     
-    Paint paint = Paint()..color = isArcHovered? Colors.blue : Colors.red;
+    
 
    Path path = Path();
-   path.arcTo(rect, 0.0, pi / 1, true);
+     
+    
+    // Radius of the arc
+    final radius = size.width / 4;
+
+    // Start angle of the arc in radians
+    final startAngle = 0.0;
+
+    // End angle of the arc in radians
+    final endAngle = 1.4; // Example: Half circle (180 degrees)
+
+    // Add the arc to the path using arcTo
+    path.arcToPoint(
+      center,
+      radius: Radius.zero,
+      rotation: endAngle - startAngle,
+      clockwise: false,
+    );
 
    if(hoverPositions.isNotEmpty){
     print("hoverPostions id not empty");
      for (var hoverPosition in hoverPositions) {
        if(path.contains(hoverPosition)){
-        isArcHovered = true;
-        print(hoverPosition);
+        hoverPositions.clear();
+        //path.arcTo(rect, 0.0, pi/1, true);
+        print("$hoverPosition I am in path.");
        }
      }
     } else {
       print('hoverpositions is empty');
     }
 
+    Paint paint = Paint()..color = isArcHovered? Colors.blue : Colors.red;
     canvas.drawPath(path, paint);
     
     
